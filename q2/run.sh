4f129af73618a166215931e2a2cec2a97f3e21ef
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-03-17 23:36:44 +0530

    printing test.c before and after doing operations

--- q2/run.sh
+++ q2/run.sh
@@ -1,5 +1,9 @@
+echo Here is the original file
+cat temp.c
 gcc -c main.c
 gcc -c r_lc.c
 gcc -c r_mc.c
 gcc -c rs.c
 gcc main.o r_lc.o r_mc.o rs.o -o out && ./out
+echo here is the file after removing comments and spaces
+cat temp.c
