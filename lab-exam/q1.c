ee1062f439ffa334f0338b3dc7555f5f4ec4e891
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-03-25 14:32:24 +0530

    added q1

---
+++ lab-exam/q1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STATE 100
+#define MAX_SYMBOLS 2
+
+
+#define mem(v,i) memset(v,i,sizeof(v)) //Use mem(v,60) to initialize with 10^9
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define repp(i,n) for(int i=(0);i<(n);i++)
+#define repr(i,n) for(int i=(n);i>=0;i--)
+#define repl(i,a,b) for(ll i=(a);i<(b);i++)
+
+#define line printf("\n===========================\n")
+#define nl printf("\n");
+
+
+
+int main(){
+    int n, s;
+
+    printf("Enter number of states");
+    scanf("%d", &n);
+
+    printf("Enter number of symbols");
+    scanf("%d", &s);
+
+
+    int dfa[n][s];
+
+    repp(i,n){
+        repp(j,s){
+            printf("Enter the index of the state reached for %d with symbol %d (enter -1 if no state is reached)", i, j);
+            scanf("%d", &dfa[i][j]);
+        }
+    }
+
+    bool isVisited[n];
+    mem(isVisited, 0);
+
+    repp(i, n){
+        repp(j, s){
+            if(dfa[i][j] != -1){
+                int k = dfa[i][j];
+                isVisited[k] = true;
+            }
+        }
+    }
+
+    line;
+    printf("States of minimised DFA:\t");
+    for(int i = 0; i < n; i++)
+        if(isVisited[i]==1)printf("%d ", i);
+
+    line;
+
+    printf("DFA Table:\n");
+    
+    printf("STATE");
+    repp(j,s)printf("\t\t%d", j);
+    nl;
+
+    repp(i, n){
+        if(isVisited[i]==0)continue;
+
+        printf("%d:", i);
+
+        repp(j, s){
+            printf("\t\t%d",dfa[i][j]);
+        }
+        nl;
+    }
+}
